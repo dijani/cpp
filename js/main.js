6f9c2a7873612ae3d1425a17e4134b437fec68c8
dijani <88719757+dijani@users.noreply.github.com>
2021-12-01 10:56:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,84 @@
+function tema_i(){
+    document.getElementById("tf").hidden=true;
+    document.getElementById("tc").hidden=true;
+    document.getElementById("tb").hidden=true;
+    document.getElementById("te").hidden=true;
+    document.getElementById("tv").hidden=true;
+    document.getElementById("ti").hidden=false;
+    document.getElementById("i").style.background='lightgray';
+    document.getElementById("f").style.background='rgb(239, 239, 239)';
+    document.getElementById("c").style.background='rgb(239, 239, 239)';
+    document.getElementById("b").style.background='rgb(239, 239, 239)';
+    document.getElementById("e").style.background='rgb(239, 239, 239)';
+    document.getElementById("v").style.background='rgb(239, 239, 239)';
+}
+function tema_f(){
+    document.getElementById("ti").hidden=true;
+    document.getElementById("tc").hidden=true;
+    document.getElementById("tb").hidden=true;
+    document.getElementById("te").hidden=true;
+    document.getElementById("tv").hidden=true;
+    document.getElementById("tf").hidden=false;
+    document.getElementById("f").style.background='lightgray';
+    document.getElementById("i").style.background='rgb(239, 239, 239)';
+    document.getElementById("c").style.background='rgb(239, 239, 239)';
+    document.getElementById("b").style.background='rgb(239, 239, 239)';
+    document.getElementById("e").style.background='rgb(239, 239, 239)';
+    document.getElementById("v").style.background='rgb(239, 239, 239)';
+}
+function tema_c(){
+    document.getElementById("tf").hidden=true;
+    document.getElementById("ti").hidden=true;
+    document.getElementById("tb").hidden=true;
+    document.getElementById("te").hidden=true;
+    document.getElementById("tv").hidden=true;
+    document.getElementById("tc").hidden=false;
+    document.getElementById("c").style.background='lightgray';
+    document.getElementById("f").style.background='rgb(239, 239, 239)';
+    document.getElementById("i").style.background='rgb(239, 239, 239)';
+    document.getElementById("b").style.background='rgb(239, 239, 239)';
+    document.getElementById("e").style.background='rgb(239, 239, 239)';
+    document.getElementById("v").style.background='rgb(239, 239, 239)';
+}
+function tema_b(){
+    document.getElementById("tf").hidden=true;
+    document.getElementById("tc").hidden=true;
+    document.getElementById("ti").hidden=true;
+    document.getElementById("te").hidden=true;
+    document.getElementById("tv").hidden=true;
+    document.getElementById("tb").hidden=false;
+    document.getElementById("b").style.background='lightgray';
+    document.getElementById("f").style.background='rgb(239, 239, 239)';
+    document.getElementById("c").style.background='rgb(239, 239, 239)';
+    document.getElementById("i").style.background='rgb(239, 239, 239)';
+    document.getElementById("e").style.background='rgb(239, 239, 239)';
+    document.getElementById("v").style.background='rgb(239, 239, 239)';
+}
+function tema_e(){
+    document.getElementById("tf").hidden=true;
+    document.getElementById("tc").hidden=true;
+    document.getElementById("tb").hidden=true;
+    document.getElementById("ti").hidden=true;
+    document.getElementById("tv").hidden=true;
+    document.getElementById("te").hidden=false;
+    document.getElementById("e").style.background='lightgray';
+    document.getElementById("f").style.background='rgb(239, 239, 239)';
+    document.getElementById("c").style.background='rgb(239, 239, 239)';
+    document.getElementById("b").style.background='rgb(239, 239, 239)';
+    document.getElementById("i").style.background='rgb(239, 239, 239)';
+    document.getElementById("v").style.background='rgb(239, 239, 239)';
+}
+function tema_v(){
+    document.getElementById("tf").hidden=true;
+    document.getElementById("tc").hidden=true;
+    document.getElementById("tb").hidden=true;
+    document.getElementById("te").hidden=true;
+    document.getElementById("ti").hidden=true;
+    document.getElementById("tv").hidden=false;
+    document.getElementById("v").style.background='lightgray';
+    document.getElementById("f").style.background='rgb(239, 239, 239)';
+    document.getElementById("c").style.background='rgb(239, 239, 239)';
+    document.getElementById("b").style.background='rgb(239, 239, 239)';
+    document.getElementById("e").style.background='rgb(239, 239, 239)';
+    document.getElementById("i").style.background='rgb(239, 239, 239)';
+}
